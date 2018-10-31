c5713adfebe90c09de9b5ac613efa7728249c851
Sagar Pednekar <sagar.pednekar@accionlabs.com>
2018-10-31 13:14:57 +0530

    chore(Added heroku mongo connection)

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 //mongo connection for heroku
 const dbconfig = {
-  url: 'mongodb://localhost:27017',
+  url: 'mongodb://test:test123@ds145573.mlab.com:45573',
   dbname: 'heroku_kbx0g42n'
 }
 const connOptions = {
